b6bd1827ea412009d6c6a0fd3c86057cd8e67395
Gabriel Borges <gabrielfr.borges@gmail.com>
2021-08-12 08:04:23 -0300

    remove unique name constraint

--- schema.sql
+++ schema.sql
@@ -3,6 +3,5 @@ create table users (
     name varchar(255) not null,
     email varchar(125) not null,
     master_password varchar(15) not null,
-    constraint UQ_USERS_NAME unique (name),
     constraint UQ_USERS_EMAIL unique (email)
 );
